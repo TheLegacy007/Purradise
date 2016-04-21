ff8a9d87255e12959be267f693dc214e2274daff
Nguyen Do <donguyenvt@yahoo.com>
2016-04-22 02:31:40 +0700

    chat

---
+++ Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Purradise
+//
+//  Created by Nguyen T Do on 4/21/16.
+//  Copyright © 2016 The Legacy 007. All rights reserved.
+//
+
+import Foundation
---
+++ Messages.swift
@@ -0,0 +1,9 @@
+//
+//  Messages.swift
+//  Purradise
+//
+//  Created by Nguyen T Do on 4/21/16.
+//  Copyright © 2016 The Legacy 007. All rights reserved.
+//
+
+import Foundation
